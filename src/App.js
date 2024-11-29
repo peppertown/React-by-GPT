15b4827cb86e9e710224a7d4496c10285e40bfa9
Dong Hyun <decidere@naver.com>
2024-11-30 01:17:23 +0900

    [Feat]: App.js 수정

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import Calendar from "./components/Calendar";
 import Quote from "./components/Quote";
 import BottomNav from "./components/BottomNav";
 import WordList from "./components/WordList";
-import EditWord from "./components/EditWord";
+import AddWordPage from "./components/AddWordPage";
+import EditWordPage from "./components/EditWordPage"; // EditWordPage import
 import "./App.css";
 
 function App() {
@@ -18,12 +19,10 @@ function App() {
       <div className="app">
         <h1>공부</h1>
         <Routes>
-          <Route path="/words/:date" element={<WordListWrapper />} />
           <Route path="/" element={<Home />} />
           <Route path="/words/:date" element={<WordListWrapper />} />
-          <Route path="/edit-word" element={<EditWord />} />{" "}
-          {/* 수정 페이지 라우트 추가 */}
-          <Route path="/" element={<Home />} />
+          <Route path="/add-word" element={<AddWordPage />} />
+          <Route path="/edit-word/:id" element={<EditWordPage />} />
         </Routes>
         <BottomNav />
       </div>
@@ -35,7 +34,7 @@ function App() {
 const Home = () => {
   return (
     <div>
-      <Quote /> {/* 홈 화면에서는 Quote 컴포넌트가 보여야 함 */}
+      <Quote />
       <Calendar />
     </div>
   );
@@ -43,10 +42,10 @@ const Home = () => {
 
 // 날짜를 prop으로 전달하는 Wrapper 컴포넌트
 const WordListWrapper = () => {
-  const { date } = useParams();
+  const { date } = useParams(); // useParams를 이용해 URL에서 날짜를 가져옵니다.
   const [year, month, day] = date.split("-").map(Number);
-  const parsedDate = new Date(year, month - 1, day); // month는 0부터 시작하므로 -1
-  return <WordList date={parsedDate} showAddButton={false} />; // Add Word 버튼 숨기기
+  const parsedDate = new Date(year, month - 1, day);
+  return <WordList date={parsedDate} />;
 };
 
 export default App;

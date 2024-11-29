bc9e31a6c628dfca8d4351551251a4a96ac50305
Dong Hyun <decidere@naver.com>
2024-11-30 01:25:16 +0900

    [Feat]: App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useParams,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Calendar from "./components/Calendar";
-import Quote from "./components/Quote";
-import BottomNav from "./components/BottomNav";
-import WordList from "./components/WordList";
-import EditWord from "./components/EditWord";
+import EditWord from "./components/EditWord"; // EditWord 컴포넌트도 유지
 import "./App.css";
 
 function App() {
@@ -18,14 +10,11 @@ function App() {
       <div className="app">
         <h1>공부</h1>
         <Routes>
-          <Route path="/words/:date" element={<WordListWrapper />} />
+          {/* 필요 없는 페이지들 삭제 */}
           <Route path="/" element={<Home />} />
-          <Route path="/words/:date" element={<WordListWrapper />} />
           <Route path="/edit-word" element={<EditWord />} />{" "}
-          {/* 수정 페이지 라우트 추가 */}
-          <Route path="/" element={<Home />} />
+          {/* EditWord 라우트 유지 */}
         </Routes>
-        <BottomNav />
       </div>
     </Router>
   );
@@ -35,18 +24,9 @@ function App() {
 const Home = () => {
   return (
     <div>
-      <Quote /> {/* 홈 화면에서는 Quote 컴포넌트가 보여야 함 */}
-      <Calendar />
+      <Calendar /> {/* Calendar 컴포넌트 유지 */}
     </div>
   );
-};
-
-// 날짜를 prop으로 전달하는 Wrapper 컴포넌트
-const WordListWrapper = () => {
-  const { date } = useParams();
-  const [year, month, day] = date.split("-").map(Number);
-  const parsedDate = new Date(year, month - 1, day); // month는 0부터 시작하므로 -1
-  return <WordList date={parsedDate} showAddButton={false} />; // Add Word 버튼 숨기기
 };
 
 export default App;
